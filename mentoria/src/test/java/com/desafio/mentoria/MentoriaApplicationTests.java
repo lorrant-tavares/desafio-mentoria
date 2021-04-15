250e9a3c752067454ed06989597ee918c696207c
Lorrant Tavares <lorrant-tavares@lorrant.tavares>
2021-04-15 08:52:15 -0300

    projeto mentoria spring

---
+++ mentoria/src/test/java/com/desafio/mentoria/MentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
